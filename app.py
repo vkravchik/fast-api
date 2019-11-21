99931378d6b95a0e6a29d26bcaded4ff7b2852cd
Vadym <vkravchik@gmail.com>
2019-11-21 21:09:21 +0200

    improve my api

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+import request
 
 import uvicorn
 
 app = FastAPI()
+
+class EditTask(BaseModel):
+    title: str
+    description: str
+    done: bool
+
+class CreateTask(BaseModel):
+    title: str
+    description: str
 
 class Task(BaseModel):
     id: int
@@ -42,3 +52,29 @@ def read_task():
 def save_task(task_id: int):
     task = list(filter(lambda t: t['id'] == task_id, tasks))
     return task[0]
+
+@app.post("/tasks", response_model=Task, status_code=201)
+def create_task(t: CreateTask):
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': t.title,
+        'description': t.description,
+        'done': False
+    }
+    tasks.append(task)
+    return task
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def edit_task(task_id, t: EditTask):
+    newTask = {
+        'id': task_id,
+        'title': t.title,
+        'description': t.description,
+        'done': t.done
+    }
+    return newTask
+
+@app.delete("/tasks/{task_id}", response_model=List[Task], status_code=201)
+def delete_task(task_id: int):
+    task = list(filter(lambda t: t['id'] != task_id, tasks))
+    return task
